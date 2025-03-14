ff29c80d5b8439662f5e79352b435e2e14d6104a
BrenoGombre <brebr7no@gmail.com>
2025-03-14 10:43:57 -0400

    allo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.send("<h1>Mon premier serveur web sur express !</h1>");
+    res.send("<h1>Mon premier serveur web surasas express !</h1>");
 });
 
 app.use('/api', recupererIdPokemon);
@@ -21,5 +21,5 @@ app.use('/api', recupererIdPokemon);
 // Démarrer le serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Serveur démarré: http://127.0.0.1:${PORT}`);
+    console.log(`Serveur démarsadsadré: http://127.0.0.1:${PORT}`);
 });
