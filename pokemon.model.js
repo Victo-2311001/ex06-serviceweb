45d85c5a3afdcb2f9c13670af295f7430d104850
BrenoGombre <brebr7no@gmail.com>
2025-03-28 10:41:54 -0400

    liste

--- pokemon.model.js
+++ pokemon.model.js
@@ -13,34 +13,28 @@ function recupererIdPokemon(id, callback){
 }
 
 //Requete qui vas chercher une liste avec des pokemons triers par un type ou toutes les pokemons
-function recupererPageListee(type, limit, offset, callback){
+function recupererPageListee(type, limit, offset, callback) {
     let requete;
     let params = [];
 
-    //Si type est definit, la requexte vas chercher une liste de pokemons avec ce meme type
-    if(type){
+    if (type) {
+        // Si un type est défini, filtrer les Pokémon par type
         requete = 'SELECT * FROM public.pokemon WHERE type_primaire = $1 LIMIT $2 OFFSET $3';
-        params = [type, limit, offset]; 
-
-        db.query(requete, params, (erreur, resultats) => {
-            if(erreur){
-                return callback(erreur);
-            }  
-            return callback(null, resultats)
-        }); 
-    }else{  
-        //Si non, la requete vas chercher une liste avec toute les pokemons
-        requete = 'SELECT * FROM public.pokemon LIMIT $2 OFFSET $3;';
-        params = [limit, offset];
-
-        db.query(requete, params, (erreur, resultats) => {
-            if(erreur){
-                return callback(erreur);
-            }  
-            return callback(null, resultats)
-        }); 
+        params = [type, limit, offset];
+    } else {
+        // Si aucun type n'est défini, récupérer tous les Pokémon
+        requete = 'SELECT * FROM public.pokemon LIMIT $1 OFFSET $2;';
+        params = [limit, offset]; // Correction des paramètres
     }
+
+    db.query(requete, params, (erreur, resultats) => {
+        if (erreur) {
+            return callback(erreur);
+        }
+        return callback(null, resultats);
+    });
 }
+
 
 const creerPokemon = (nom, type1, type2, pv, attaque, defense) => {
     return new Promise ((resolve, reject) => {
